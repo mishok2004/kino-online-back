432b44b8e17b8064c5bd02c3ba471b17260aef1d
mishok2004 <mishok2004@mail.ru>
2025-01-16 22:56:00 +0300

    install nest

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { AuthService } from './auth.service'
+import { Body, Controller, Post } from '@nestjs/common'
+import { register } from 'module'
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly AuthService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() dto: any) {
+    return this.AuthService.register(dto)
+  }
+}
