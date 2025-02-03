409401426396e8a69b9c4cd67aa3fd68c9063aa8
mishok2004 <144279964+mishok2004@users.noreply.github.com>
2025-02-03 23:28:38 +0300

    add work users profile

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,9 @@
 import { Auth } from 'src/auth/decorators/auth.decorator';
 import { UserService } from './user.service';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { User } from './decorators/user.decorator';
+import { UpdateUserDto } from './dto/updateuser.dto';
+import { IdValidationPipe } from 'src/pipes/id.validation.pipe';
 
 @Controller('users')
 export class UserController {
@@ -12,4 +14,50 @@ export class UserController {
     async getProfile(@User('_id') _id: string) {
         return this.userService.byID(_id)
     }
+
+    @UsePipes(new ValidationPipe())
+    @Put('profile')
+    @HttpCode(200)
+    @Auth()
+    async updateProfile(@User('_id') _id: string, @Body() dto: UpdateUserDto) {
+        return this.userService.updateProfile(__dirname, dto)
+    }
+
+
+    @Get()
+    @Auth('admin')
+    async getUsers(@Query('searchTerm') searchTerm?: string) {
+        return this.userService.getAll(searchTerm)
+    }
+
+    @Get(':id')
+    @Auth('admin')
+    async getUser(@Param('_id', IdValidationPipe) id: string) {
+        return this.userService.byID(id)
+    }
+
+
+    @Get('count')
+    @Auth('admin')
+    async getCountUsers() {
+        return this.userService.getCount()
+    }
+
+    @UsePipes(new ValidationPipe())
+    @Put(':id')
+    @HttpCode(200)
+    @Auth('admin')
+    async updateUser(@Param('id', IdValidationPipe) id: string, @Body() dto: UpdateUserDto) {
+        return this.userService.updateProfile(id, dto)
+    }
+
+
+
+    @Delete(':id')
+    @HttpCode(200)
+    @Auth('admin')
+    async deleteUser(@Param('id', IdValidationPipe) id: string) {
+        return this.userService.delete(id)
+    }
+
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,11 +2,12 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from 'nestjs-typegoose';
 import { UserModel } from './user.model';
 import { ModelType } from '@typegoose/typegoose/lib/types'
+import { UpdateUserDto } from './dto/updateuser.dto';
+import { genSalt, hash } from 'bcryptjs';
 
 
 @Injectable()
 export class UserService {
-
     constructor(@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>) { }
 
 
@@ -16,6 +17,50 @@ export class UserService {
 
 
         return user
+    }
+
+
+    async updateProfile(_id: string, dto: UpdateUserDto) {
+        const user = await this.byID(_id)
+        const isSameUser = await this.UserModel.findOne({ email: dto.email })
+
+        if (isSameUser && String(_id) !== String(isSameUser._id)) throw new NotFoundException('User is registered')
+
+
+        if (dto.password) {
+            const salt = await genSalt(10)
+            user.password = await hash(dto.password, salt)
+        }
+
+        user.email = dto.email
+        if (dto.isAdmin || dto.isAdmin === false)
+            user.isAdmin = dto.isAdmin
+
+        await user.save()
+        return
+    }
+
+    async getCount() {
+        return this.UserModel.find().count().exec()
+    }
+
+    async getAll(searchTerm?: string) {
+        let options = {}
+
+        if (searchTerm) options = {
+            $or: [
+                {
+                    email: new RegExp(searchTerm, 'i')
+                }
+            ]
+        }
+        return this.UserModel.find(options).select('-password -updateAt -__v').sort({
+            createdAt: 'desc'
+        }).exec()
+    }
+
+    async delete(id: string) {
+        return this.UserModel.findOneAndDelete().exec()
     }
 
 }
